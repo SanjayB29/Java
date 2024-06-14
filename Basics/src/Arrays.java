78a610d0c4737fa1da33fbd6b0f9566b6dc7587c
Sanjay Balamurugan <sanjayb2903@gmail.com>
2024-06-14 12:49:57 +0530

    Arrays

---
+++ Basics/src/Arrays.java
@@ -0,0 +1,6 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] names= {"Sanjay", "Balamurugan"};
+        System.out.println(names[1]+" "+names.length);
+    }
+}
